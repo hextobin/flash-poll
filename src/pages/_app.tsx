dde3fd1eac659e042ece9c72aecd46c249a95879
hextobin <spencer@databit.io>
2023-12-03 17:32:06 -0700

    add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link rel="icon" href="./fav.svg" type="image/svg+xml" />
+        <title>
+          {/* catchphrase to describe the website */}
+          FlashPoll-Make and Share Polls
+        </title>
       </Head>
       <RootLayout>
         <Component {...pageProps} />
