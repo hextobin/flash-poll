49a94fcaf08c05917528af2c0b8e6062e4a9dbec
hextobin <spencer@databit.io>
2023-12-03 17:28:56 -0700

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { AppProps } from "next/app";
 import RootLayout from "../components/RootLayout";
-import "../public/globals.css";
+import "../../public/globals.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RootLayout>
-      <Component {...pageProps} />
-    </RootLayout>
+    <>
+      <Head>
+        <link rel="icon" href="./fav.svg" type="image/svg+xml" />
+      </Head>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    </>
   );
 }
